d28a6e2699e3910fd3e7a40313e39f30a5c146b6
Dmitrii Fediuk <dfediuk@gmail.com>
2024-03-10 18:47:01 +0100

    ⠀

--- lib/category.php
+++ lib/category.php
@@ -2,10 +2,12 @@
 use Magento\Catalog\Model\Category as C;
 /**
  * 2024-03-10
- * Top-level categories:
+ * 1.1) Level 0: «Root Catalog».
+ * 1.2) Level 1: «Default Category».
+ * 1.3) Level 2:
  * 		«Ready to Assemble Cabinets»
  * 		«Pre-Assembled Cabinets»
  * 		«Cabinet Organizers & Hardware»
  * @used-by app/design/frontend/Cabinetsbay/cabinetsbay_default/Magento_Catalog/templates/category/header.phtml
  */
-function cb_category_is_top(C $c):bool {return 2 === df_category_level($c);}
+function cb_category_is_l2(C $c):bool {return 2 === df_category_level($c);}
